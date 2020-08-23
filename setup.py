b9f72ade149d16218d88f46ee2936028eb0df0ce
Andres Vazquez <andres@data99.com.ar>
2020-08-23 12:03:22 -0300

    Fix CI error

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='''siu-data''',
-    version='0.2.12',
+    version='0.2.13',
     description='''SIU data''',
     long_description=long_description,
     long_description_content_type='text/markdown', 
